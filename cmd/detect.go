2e416f3447329f28a2f4dd18105f16af8b7fc82d
rockerBOO <rockerboo@gmail.com>
2020-12-12 23:34:33 -0500

    Detects on my computer

--- cmd/detect.go
+++ cmd/detect.go
@@ -16,16 +16,21 @@ var detectCmd = &cobra.Command{
 	Long: `Detect all scene files in the common user locations. 
 			~/.config/obs-studio/basic/scenes`,
 	Run: func(cmd *cobra.Command, args []string) {
-		directories := []string{"~/.config/obs-studio/basic/scenes"}
-		for _, detect := range Detect(directories) {
-			if len(detect) == 0 {
+		directories := []string{filepath.Dir("/home/rockerboo/.config/obs-studio/basic/scenes/")}
+
+		fmt.Printf("directories: %+v\n", directories)
+
+		detected_files := Detect(directories)
+
+		for _, file := range detected_files {
+			if len(file) == 0 {
 				continue
 			}
 
 			fmt.Println("Detected this file: ")
-			fmt.Println(detect)
+			fmt.Println(file)
 
-			x, _ := unwrap([]byte(detect))
+			x, _ := unwrap([]byte(file))
 
 			fmt.Println(x)
 		}
@@ -41,48 +46,99 @@ func Detect(directories []string) []string {
 	found_files := make([]string, 1)
 
 	for _, dir := range directories {
-		found, file := find_in_directory(dir)
+		fmt.Printf("find in directory %+v\n", dir)
+		is_found, file := find_in_directory(dir)
 
-		if found {
+		if !is_found {
+			fmt.Printf("no file found %+v\n", dir)
+			continue
+		}
+
+		sources, err := find_sources(file)
+
+		if err != nil {
+			fmt.Printf("no sources found %+v\n", err)
+		}
+
+		for _, source := range sources {
+			fmt.Printf("source found %+v\n", source.Name)
+		}
+
+		if is_found {
 			found_files = append(found_files, file)
 		}
 	}
 
+	fmt.Printf("%+v\n", found_files)
+
 	return found_files
+}
+
+type Scenes struct {
+	Sources []Source
+}
+
+type Source struct {
+	Id      string
+	Name    string
+	Enabled bool
+}
+
+func open_json(file string, unpack_json Scenes) (*Scenes, error) {
+	file_source, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		fmt.Printf("%v %+v\n", file, err)
+		return &Scenes{}, err
+	}
+
+	if err := json.Unmarshal(file_source, &unpack_json); err != nil {
+		fmt.Printf("invalid unmarshal for %+v %+v\n", file, err)
+		return &Scenes{}, err
+	}
+
+	return &unpack_json, nil
+}
+
+func find_sources(file string) ([]Source, error) {
+	scenes, err := open_json(file, Scenes{})
+
+	if err != nil {
+		return []Source{}, err
+	}
+
+	return scenes.Sources, nil
 }
 
 // Find any scenes files in the directory
 func find_in_directory(directory string) (bool, string) {
-	dir := filepath.Dir(directory)
+	dir := filepath.Dir(directory + "/")
+
+	println(dir)
+
 	list, err := ioutil.ReadDir(dir)
+
+	fmt.Printf("find_in_directory %+v\n", err)
 
 	if err != nil {
 		return false, ""
 	}
 
 	for _, file := range list {
-		if !file.IsDir() {
+		fmt.Printf("file in list %+v\n", dir+file.Name())
+		if file.IsDir() {
+			fmt.Printf("is a dir %+v\n", dir+file.Name())
 			continue
 		}
 
 		if strings.HasSuffix(file.Name(), ".json") {
-			return true, file.Name()
+			fmt.Printf("Found file %+v\n", dir+file.Name())
+			return true, filepath.Join(dir, file.Name())
 		}
 	}
 
 	return false, ""
 }
-
-// Find any scenes in the file
-// func find(file string) (string, error) {
-// 	f, err := os.Open(file)
-
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	return file.
-// }
 
 func unwrap(dat []byte) (map[string]interface{}, error) {
 	var jsoned map[string]interface{}
